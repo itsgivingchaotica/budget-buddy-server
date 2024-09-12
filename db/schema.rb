7d72f456eb466cf1088aee7868880071cc536fdb
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2024-09-11 21:37:18 -0400

    update entries and budget tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_28_190310) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_07_152907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_28_190310) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category_type"
   end
 
   create_table "entries", force: :cascade do |t|
@@ -66,10 +67,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_28_190310) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "mobile_number"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   add_foreign_key "budgets", "users"
