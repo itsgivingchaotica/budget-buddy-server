ae94d2a4771a7a4914200416630950f9f4f7c985
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2025-04-20 17:46:40 -0400

    establish further relationships in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :budgets, :entries_tags, dependent: :destroy
-
+  has_many :budgets, class_name: 'Budget', foreign_key: :user_id, dependent: :destroy
+  has_many :entries_tags, dependent: :destroy 
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 end
